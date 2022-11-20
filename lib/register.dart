b46d17dc7926d702e746cead82042e06a9c8d5f4
SpinevAndrew <last.name.spinev@gmail.com>
2022-11-21 00:03:33 +0300

    Add number current lesson in registration

--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -204,6 +205,9 @@ class RegisterState extends State {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: email, password: password);
      await FirebaseAuth.instance.currentUser?.updateDisplayName(registerData.username);
+     FirebaseFirestore.instance.collection("users").doc("${FirebaseAuth.instance.currentUser?.uid}").set(
+        {"currentLesson": 1}
+     );
 
     }
     on FirebaseAuthException catch (e){
